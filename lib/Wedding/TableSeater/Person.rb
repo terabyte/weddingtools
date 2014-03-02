5422cd1ead06a678d40404a21735cd76a9471caa
Carl Myers <cmyers@palantir.com>
2014-03-02 12:52:01 -0800

    Initial commit

---
+++ lib/Wedding/TableSeater/Person.rb
@@ -0,0 +1,13 @@
+
+module Wedding
+    module TableSeater
+        class Person
+            attr_accessor :id
+            attr_accessor :name
+            attr_accessor :group_list
+
+            def get_affinity_to_person(other)
+            end
+        end
+    end
+end
